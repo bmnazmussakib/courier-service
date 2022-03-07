4db27cd7728f24451fc6c32d51d29dd0dc66c7dc
bmnazmussakib <nazmussakib161@gmail.com>
2022-03-07 21:19:48 +0600

    carousel added in testimonial

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-  $('.counter').counterUp({
-    delay: 100,
-    time: 1000
-  });
+    $('.counter').counterUp({
+        delay: 100,
+        time: 1000
+    });
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true,
+        nav: true
+    });
+    $('.play').on('click', function() {
+        owl.trigger('play.owl.autoplay', [1000])
+    })
+    $('.stop').on('click', function() {
+        owl.trigger('stop.owl.autoplay')
+    })
 
 
 });
